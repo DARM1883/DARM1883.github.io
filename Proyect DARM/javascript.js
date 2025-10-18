dea017e5b4cc7be425ef308c7a9df0b75db814ae
DARM1883 <dr943724@gmail.com>
2025-10-17 20:58:21 -0600

    Add files via upload

---
+++ Proyect DARM/javascript.js
@@ -0,0 +1,15 @@
+
+function Alerta(){
+alert("Segundo mensaje de alerta - mensaje en js")
+}
+function impresion(){
+    window.print();
+}
+
+function Youtube(){
+    window.open('https://www.youtube.com')
+}
+
+ function enviar(){
+        window.location.href= 'tabla de investigacion.html'
+    }
